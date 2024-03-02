4e74bce892826592845dc447dadd2eab62953396
Tiago Antunes <tiagoantunes7@gmail.com>
2024-03-02 02:43:33 +0000

    Initial Commit

---
+++ PokemonLibrary/PokemonLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonLibraryApp.swift
+//  PokemonLibrary
+//
+//  Created by Tiago Antunes on 02/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
